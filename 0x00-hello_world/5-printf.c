9ef7a2cc754afc26bbded79d587581db6b9f8a9c
Elmustafa <mostabkor@gmail.com>
2023-09-10 15:02:20 -0700

    eoek

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-	/**
-	 * main - this is the main function
-	 * description - starting of the program
-	 */
+/**
+ * main - this is the main function
+ * descrption - the program start here
+*/
 	int main(void)
 	{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
