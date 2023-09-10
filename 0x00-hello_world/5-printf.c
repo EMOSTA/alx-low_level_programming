52c8212999e36a61c5430f11868aec0657eecc01
Elmustafa <mostabkor@gmail.com>
2023-09-10 14:51:59 -0700

    einwaj

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
+	
 	/**
 	 * function - main
-	 * discrption - starting of the program
+	 * description - starting of the program
 	 */
+	
 	int main(void)
 	{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
