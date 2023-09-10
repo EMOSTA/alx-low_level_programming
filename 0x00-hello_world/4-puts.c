f7a878389e1dce44a6789821459d926c67732e2e
Elmustafa <mostabkor@gmail.com>
2023-09-10 14:25:56 -0700

    eowkw

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 	
-	//Programming is like building a multilingual puzzle
-	
+	/**
+	 * Programming is like building a multilingual puzzle
+	 */
 	int main(void)
 	{
 	puts("\"Programming is like building a multilingual puzzle");
