67c2cf38583256871cd0af621cb500d10a9212a3
Elmustafa <mostabkor@gmail.com>
2023-09-10 17:45:21 -0700

    eidj

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,12 +3,12 @@
  * main - the programstart 
  * Return - alwyays 0
  */
-int main()
+int main(v)
 	{
-	printf("size of a char: byte(s) %1d\n", sizeof(char));
-	printf("size of an int: byte(s) %1d\n", sizeof(int));
-	printf("size of a long int: byte(s) %1d\n", sizeof(long int));
-	printf("size of a along long int: byte(s) %1d\n", sizeof(long long int));
-	printf("size of a float: byte(s) %1d\n", sizeof(float));
+	printf("size of a char: byte(s) %c\n", sizeof(char));
+	printf("size of an int: byte(s) %d\n", sizeof(int));
+	printf("size of a long int: byte(s) %ld\n", sizeof(long int));
+	printf("size of a long long int: byte(s) %lld\n", sizeof(long long int));
+	printf("size of a float: byte(s) %1f\n", sizeof(float));
 return (0);
 	}
