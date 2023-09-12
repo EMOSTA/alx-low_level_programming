8e62e0bb189d4fce5c90101f62d2a10f4b5c376c
Elmustafa <mostabkor@gmail.com>
2023-09-11 19:11:33 -0700

    eodke

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - finction
+ *
+ * Return: alwayes 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z' ; a >= 'a' ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
