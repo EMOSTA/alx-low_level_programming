8155b4378a66a6c4828c336a5526f7a9698ba1b4
Elmustafa <mostabkor@gmail.com>
2023-09-11 19:01:32 -0700

    eidm

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main _ function
+ *
+ * Return: alwayes 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
