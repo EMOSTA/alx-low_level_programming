add4053ebd96dc341d66082b5b3eb9f84f7464fd
Elmustafa <mostabkor@gmail.com>
2023-09-12 18:15:21 -0700

    eok

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+nclude "main.h"
+
+/**
+ *  * main - prints alphabet to stdout,
+ *   * Description: uses print_alphabet function to print alphabet
+ *    * Return: Always returns 0
+ *    */
+int main(void)
+{
+	    print_alphabet();
+	        return (0);
+}
