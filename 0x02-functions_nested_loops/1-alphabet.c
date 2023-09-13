dd02b4da401a7c4aa7f19cd7b9d67c83010844b2
Elmustafa <mostabkor@gmail.com>
2023-09-12 18:17:13 -0700

    eok

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+
+/**
+ * print_alphabet - prints alphabet to stdout,
+ * Description: prints alphabet in lowercase using _putchar function.
+ * Return: Always Returns Nothing
+ */
+void print_alphabet(void)
+{
+		const char *alphabet = "abcdefghijklmnopqrstuvwxyz";
+			int i                = 0;
+
+				while (alphabet[i] != '\0')
+						{
+									_putchar(alphabet[i]);
+											i++;
+												}
+					_putchar('\n');
+}
