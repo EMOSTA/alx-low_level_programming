5977d2eab4dab2d4006ce158e5a19f6f85a1d735
Elmustafa <mostabkor@gmail.com>
2023-09-12 18:30:01 -0700

    eij

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *     main - print alphabet 10 times,
+ *         Description: print alphabet 10 times using 2-print_alphabet_x10 function.
+ *             Return: Always 0
+ *             */
+int main(void)
+{
+	    print_alphabet_x10();
+	        return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets 10 times,
+ * Description: prints alphabets 10 times using print_alphabet.
+ * Return: Always Returns Nothing
+ */
+void print_alphabet_x10(void)
+{
+		const char *alphabet    = "abcdefghijklmnopqrstuvwxyz";
+			int i, j                = 0;
+
+				for (j = 0; j < 10; j++)
+						{
+		while (alphabet[i] != '\0')
+												{
+																_putchar(alphabet[i]);
+																			i++;
+																					}
+											i = 0;
+													_putchar('\n');
+														}
+}
+
