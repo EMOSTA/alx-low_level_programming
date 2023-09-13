2820921a1715f4a0226e057a589c16df761968c1
Elmustafa <mostabkor@gmail.com>
2023-09-12 17:23:02 -0700

    oekk

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include<stdio.h>
+#include "main.h"
+
 /**
- *  * main - function
- *   *
- *    * Return: alwayes 0
- *     */
+ *  * main - writing to stdout,
+ *   * Description: writing to stdout using _putchar
+ *    * Return: return 0 upon success
+ */
 int main(void)
 {
-		printf("_putchar \n");
-			return (0);
+		_putchar('_');
+			_putchar('p');
+				_putchar('u');
+					_putchar('t');
+						_putchar('c');
+							_putchar('h');
+								_putchar('a');
+									_putchar('r');
+										_putchar('\n');
+											return (0);
 }
