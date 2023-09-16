8a48f22e507470cac9e559f18f987f5c4603e165
Elmustafa <mostabkor@gmail.com>
2023-09-15 18:51:36 -0700

    eiodkawd

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,11 +8,14 @@
  * Return: 0 or 1
  */
 
-int _isupper(int c)
+int _isupper(int c);
+
 {
-		if (c >= 65 && c <= 90)
-					return (1);
-			else
-						return (0);
-i}
+
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+
+}
 
